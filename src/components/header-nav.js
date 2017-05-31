ae3e15cb5b501542328208e863e9991a655a93f0
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-05-31 04:15:49 +0100

    issue with refreshing routes

--- src/components/header-nav.js
+++ src/components/header-nav.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HeaderNav= () => {
   return (
-    <h2>Header Nav</h2>
+    <nav>
+        <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+    </nav>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
-import reducers from './reducers';
+// import reducers from './reducers';
 import HeaderNav from './components/header-nav';
 import Home from './components/home';
 import About from './components/about';
 import Contact from './components/contact';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter>
-      <div>
-        <Switch>
-          <Route exact path="/" component={ Home } />
-          <Route path="/about" component={ About } />
-          <Route path="/contact" component={ Contact } />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  </Provider>
-, document.querySelector('.app'));
+const App = ()=> (
+  <div>
+    <HeaderNav />
+    <Main />
+  </div>
+)
+
+const Main =()=>(
+  <div>
+    <Switch>
+      <Route exact path="/" component={ Home } />
+      <Route path="/about" component={ About } />
+      <Route path="/contact" component={ Contact } />
+    </Switch>
+  </div>
+)
+
+ReactDOM.render((
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>
+), document.querySelector('.app'));
