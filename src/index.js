795faeeae7c6efc58b6301c848487b588183b94f
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2018-01-20 08:37:43 +0000

    correct icon sizez

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // webpack entry point
 
 // Use webfontloader to import google font(s).
-import WebFont from 'webfontloader';
+// import WebFont from 'webfontloader';
 
-WebFont.load({
-  google: {
-    families: ['Open Sans']
-  }
-});
+// WebFont.load({
+//   google: {
+//     families: ['Open Sans']
+//   }
+// });
 
 import "./scss/master.scss";
 import "./reactApp";
